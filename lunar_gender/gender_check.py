8673b8227e70dad8441a7e9d5d12c3ea1a1e0d40
sherwinstar <slzhouzsl@163.com>
2022-04-08 14:53:51 +0800

    wrap apis

--- lunar_gender/gender_check.py
+++ lunar_gender/gender_check.py
@@ -7,56 +7,106 @@ from predict.gender_predict import GenderPredict
 
 
 class GenderCheck:
-    def __init__(self, argv):
-        if not isinstance(argv, list) or len(argv) == 0:
-            print('input is not right')
-            sys.exit(2)
+    def __init__(self, argv, script=False):
+        self.status = -1
+        self.age = ''
+        self.gender = ''
+        self.date = ''
+        self.birthday = None
+        self.date_genders = []
+        date_str = ''
+        if script:
+            if not isinstance(argv, list) or len(argv) == 0:
+                print('input is not right')
+                return
+                pass
+            date_str = argv[0]
             pass
-        date_str = argv[0]
+        else:
+            if not isinstance(argv, str):
+                print('input is not right')
+                return
+                pass
+            date_str = argv
+            pass
         dates = date_str.split('.')
         if len(dates) != 3:
             print('input is not right')
-            sys.exit(2)
+            return
             pass
         western_birthday = None
         try:
             western_birthday = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
         except Exception as e:
             print('input is not right')
-            sys.exit(2)
+            return
         pass
 
         if western_birthday is None:
             print('input is not right')
-            sys.exit(2)
+            return
             pass
         today = datetime.today()
         if western_birthday > today:
             print("the input date is later than today")
-            exit(0)
+            return
             pass
-        self.check_gender(western_birthday)
+        self.birthday = western_birthday
         pass
 
-    pass
+    def check_pregrancy_gender(self, type=0):
+        if self.birthday is None:
+            return
+            pass
+        if type == 0:
+            self.check_gender()
+            pass
+        elif type == 1:
+            self.check_genders()
+        pass
 
-    def check_gender(self, date: datetime):
-        gender_predict = GenderPredict(date)
+    def check_gender(self):
+        gender_predict = GenderPredict(self.birthday)
         age, date, gender = gender_predict.check()
         if len(gender):
+            self.status = 0
+            self.date = date.strftime('%B %Y')
+            self.age = str(age)
+            self.gender = gender
+            print(self.age + ", " + self.date + ", " + self.gender)
+            pass
+        pass
+
+    def check_genders(self):
+        gender_predict = GenderPredict(self.birthday)
+        date_genders = gender_predict.check_next_12_month()
+        if len(date_genders):
+            self.status = 0
+            pass
+        for dic in date_genders:
+            date: datetime = dic['date']
+            gender = dic['gender']
+            age = dic['age']
             date_str = date.strftime('%B %Y')
-            print(str(age) + ", " + date_str + ", " + gender)
+            self.date_genders.append({'age': age, 'date': date_str, 'gender': gender})
+            if len(gender):
+                print(str(age) + ", " + date_str + ", " + gender)
+                pass
+            else:
+                print(str(age) + ", " + date_str + ", we cannot predict")
+                pass
             pass
         pass
 
     pass
+
 
 def test2():
     date = datetime(1990, 1, 18)
     gender_predict = GenderPredict(date)
     date_genders = gender_predict.check_next_12_month()
     for dic in date_genders:
-        date:datetime = dic['date']
+        date: datetime = dic['date']
         gender = dic['gender']
         age = dic['age']
         date_str = date.strftime('%B %Y')
@@ -85,6 +135,7 @@ def test():
 if __name__ == '__main__':
     # python3 gender_check.py 1915.2.28
     # test()
-    GenderCheck(sys.argv[1:])
-
+    check = GenderCheck(sys.argv[1:], True)
+    # check = GenderCheck('1995.1.28')
+    check.check_pregrancy_gender(0)
     exit(0)
---
+++ lunar_gender/predict_api.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# _*_ coding: UTF-8 _*_
+
+
+from flask import Flask, request
+from gender_check import GenderCheck
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/child_gender', methods=['GET'])
+def child_gender():
+    type = request.args.get('type', '0')
+    birthday = request.args.get('birthday', '')
+    check = GenderCheck(birthday)
+    if type == '0':
+        check.check_pregrancy_gender(0)
+        if check.status == 0:
+            data = {'age': check.age, 'date': check.date, 'gender': check.gender}
+            return {
+                "msg": "success",
+                "status": 0,
+                "data": data
+            }
+        else:
+            data = {}
+            return {
+                "msg": "error",
+                "status": -1,
+                "data": data
+            }
+        pass
+    elif type == '1':
+        check.check_pregrancy_gender(1)
+        if check.status == 0:
+            data = check.date_genders
+            return {
+                "msg": "success",
+                "status": 0,
+                "data": data
+            }
+        else:
+            data = []
+            return {
+                "msg": "error",
+                "status": -1,
+                "data": data
+            }
+        pass
+    else:
+        return {
+            "msg": "error",
+            "status": -1
+        }
+    pass
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Predict the child gender based on lunar age of mother\'s birthday.'
+    pass
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
